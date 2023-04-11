48046761f9ebeb6d551861abc5b42c151919894d
rejauldu@gmail.com <rejauldu@gmail.com>
2023-04-11 15:15:30 +0600

    Teleport added

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,22 @@
 <template>
   <!-- <TodoList /> -->
   <!-- <WatchExample /> -->
-  <VModelExample />
+  <!-- <VModelExample /> -->
+  <TeleportExample />
 </template>
 
 <script>
 // import TodoList from './components/TodoList.vue'
 // import WatchExample from './components/WatchExample.vue'
-import VModelExample from './components/v-model/VModelExample.vue'
+// import VModelExample from './components/v-model/VModelExample.vue'
+import TeleportExample from './components/teleport/TeleportExample.vue'
 export default {
   name: 'App',
   components: {
-    // TodoList
-    // WatchExample
-    VModelExample
+    // TodoList,
+    // WatchExample,
+    // VModelExample,
+    TeleportExample
   }
 }
 </script>
---
+++ src/components/teleport/TeleportExample.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="teleport">
+        <h1>Teleport Example</h1>
+        <Teleport to="#destination">
+            <h1>Under destination</h1>
+        </Teleport>
+    </div>
+</template>
+<script>
+import { Teleport } from 'vue';
+components: {
+    Teleport
+}
+export default {
+    
+}
+</script>
+<style scoped>
+    .teleport {
+        border: 5px solid gray;
+        width: 500px;
+        height: 300px;
+        margin: auto;
+    }
+    h1 {
+        text-align: center;
+    }
+</style>
+<style>
+    #destination {
+        border: 3px solid tomato;
+    }
+</style>
